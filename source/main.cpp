3192aed5e2653f3225ffa8b5096c6a764397473f
WujekFoliarz <igorpodstawek14@gmail.com>
2025-06-15 18:18:12 +0200

    add DllMain entry

--- source/main.cpp
+++ source/main.cpp
@@ -4,4 +4,24 @@
 int main(int argc, char* argv[]) {
 	return 0;
 }
+#elif COMPILE_TO_DLL == 1 && defined(_WIN32)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    switch (fdwReason) {
+        case DLL_PROCESS_ATTACH:
+            break;
+
+        case DLL_THREAD_ATTACH:
+            break;
+
+        case DLL_THREAD_DETACH:
+            break;
+
+        case DLL_PROCESS_DETACH:
+            if (lpvReserved != nullptr) {
+                break;
+            }
+            break;
+    }
+    return TRUE;
+}
 #endif
