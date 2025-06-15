9eb205f72847da6b16ad601e44332eb0e1892709
WujekFoliarz <igorpodstawek14@gmail.com>
2025-06-15 20:23:40 +0200

    change DLL_PROCESS_DETACH case

--- source/main.cpp
+++ source/main.cpp
@@ -20,9 +20,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 		}
 
 		case DLL_PROCESS_DETACH: {
-			if (lpReserved != nullptr) {
-				break;
-			}
 			break;
 		}
 	}
